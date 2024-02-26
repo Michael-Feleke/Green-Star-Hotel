cafdf89b23cdc85c6e635f6ed8a4bad774df74ef
Michael Feleke <felekemichael999@gmail.com>
2024-02-26 11:30:18 +0300

    creating an api to fetch rooms from supabase

--- src/services/apiRooms.js
+++ src/services/apiRooms.js
@@ -1,1 +1,12 @@
-export async function getRooms() {}
+import supabase from "./supabase";
+
+export async function getRooms() {
+  const { data, error } = await supabase.from("Rooms").select("*");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Rooms could not be loaded");
+  }
+
+  return data;
+}
