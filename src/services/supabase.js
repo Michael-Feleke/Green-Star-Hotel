de04d27838647aaf81daacf20cde03b5b37c90e4
Michael Feleke <felekemichael999@gmail.com>
2024-02-26 11:17:49 +0300

    creating supabase js to connect the application with database

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://jxdyxaoiqjnzjphbqtfu.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp4ZHl4YW9pcWpuempwaGJxdGZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDg5MzA4OTUsImV4cCI6MjAyNDUwNjg5NX0.Q-gEehaOJFl-7yGH5k83koP2RRt44bB_TLX3KFNQlD8";
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
