7babeaf0c2fd29763b47bdd58948f2bfe4147b58
Michael Feleke <felekemichael999@gmail.com>
2024-02-27 19:55:22 +0300

    setting up the toaster

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
 import Rooms from "./pages/Rooms";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
@@ -39,6 +40,26 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 5000,
+          },
+          error: {
+            duration: 7000,
+          },
+          style: {
+            fontSize: "16px",
+            maxWidth: "500px",
+            padding: "16px 24px",
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-7000)",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
